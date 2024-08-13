dc83c8737f5eec2fe2eac38b503c6eb861e4637c
Gaya Lee <ohhappy73day@gmail.com>
2024-08-13 13:32:38 +0900

    branch 생성

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const apiController = require('./controller');
 const app = express();
 app.use(express.json());
 app.use(express.static("public"));
+console.log("exec express");
 
 app.use("/api", apiController);
 
