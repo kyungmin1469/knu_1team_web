84ea50dfd773d2e0da188277048ba3b2cd054279
codingle2 <kpss0337@gmail.com>
2024-08-14 16:38:28 +0900

    commit

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -1,26 +1,14 @@
 const productController = require("express").Router();
+const Product = require("../schema/product.schema");
+const getProductList = require("../service/product.service");
 
 // 상품 조회 api
 // 1. 전부 all 뽑기
 // 2. filter(조건)
 // 3. 페이지 네이션 적용 -> list index
-
-const dummyData = Array.from({ length: 30 }, (_, index) => {
-  const product = {
-    _id: index,
-    title: `product-title-${index + 1}`,
-    price: Math.floor(Math.random() * 90000) + 10000,
-    description: `product-title-${
-      index + 1
-    } 에 대한 desription이 들어가는 layout 입니다.`,
-    imgUrl: `https://picsum.photos/id/${index + 1}/200/300`,
-    stock: Math.ceil(Math.random() * 100),
-  };
-  return product;
-});
-
 productController.get("/", (req, res) => {
-  return res.json({ result: true, data: dummyData });
+  const productlist = getProductList();
+  return res.json({ result: true, data: productlist });
 });
 
 module.exports = productController;
